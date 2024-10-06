5ec4d6ae80c64de6ccd05c9836fea778a50820a4
mahomuda_mim <mahomudamim@gmail.com>
2024-10-06 23:37:16 +0600

    Update

--- bmSystem/urls.py
+++ bmSystem/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from myapp import views as myapp_views
 from myapp import views as buy_books_views
+from myapp import views as login_signin_subscription
+from myapp import views as subscribed_user
+from myapp import views as login_user
+from myapp.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +34,24 @@ urlpatterns = [
     path('novel/', buy_books_views.novel, name='novel'),
     path('thriller/', buy_books_views.thriller, name='thriller'),
     path('poetry/', buy_books_views.poetry, name='poetry'),
+
+    path('login/', login_signin_subscription.login, name='login'),
+    path('signin/', login_signin_subscription.signin, name='signin'),
+    path('subscription/', login_signin_subscription.subscription, name='subscription'),
+    path('forget_pass/', login_signin_subscription.forget_pass, name='forget_pass'),
+    path('login_with/', login_signin_subscription.login_with, name='login_with'),
+    path('payment/', login_signin_subscription.payment, name='payment'),
+
+    path('u_help/', subscribed_user.u_help, name='u_help'),
+    path('sub_profile/', subscribed_user.sub_profile, name='profile'),
+    path('rented_books/', subscribed_user.rented_books, name='rented_books'),
+    path('sub_base/', subscribed_user.sub_base, name='sub_base'),
+    path('sub_navbar/', subscribed_user.sub_navbar, name='sub_navbar'),
+
+    path('log_help/', login_user.log_help, name='log_help'),
+    path('log_profile/', login_user.log_profile, name='log_profile'),
+    path('log_books/', login_user.log_books, name='log_books'),
+    path('log_base/', login_user.log_base, name='log_base'),
+    path('log_navbar/', login_user.log_navbar, name='log_navbar'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -23,3 +23,41 @@ def thriller(request):
     return render(request, template_name='Buy_Books/thriller.html')
 def poetry(request):
     return render(request, template_name='Buy_Books/poetry.html')
+
+
+def login(request):
+    return render(request, template_name='login_signin_subscription/login.html')
+def signin(request):
+    return render(request, template_name='login_signin_subscription/signin.html')
+def subscription(request):
+    return render(request, template_name='login_signin_subscription/subscription.html')
+def forget_pass(request):
+    return render(request, template_name='login_signin_subscription/forget_pass.html')
+def login_with(request):
+    return render(request, template_name='login_signin_subscription/login_with.html')
+def payment(request):
+    return render(request, template_name='login_signin_subscription/payment.html')
+
+
+def u_help(request):
+    return render(request, template_name='subscribed_user/u_help.html')
+def sub_profile(request):
+    return render(request, template_name='subscribed_user/sub_profile.html')
+def rented_books(request):
+    return render(request, template_name='subscribed_user/rented_books.html')
+def sub_navbar(request):
+    return render(request, template_name='subscribed_user/sub_navbar.html')
+def sub_base(request):
+    return render(request, template_name='subscribed_user/sub_base.html')
+
+
+def log_base(request):
+    return render(request, template_name='login_user/log_base.html')
+def log_books(request):
+    return render(request, template_name='login_user/log_books.html')
+def log_help(request):
+    return render(request, template_name='login_user/log_help.html')
+def log_navbar(request):
+    return render(request, template_name='login_user/log_navbar.html')
+def log_profile(request):
+    return render(request, template_name='login_user/log_profile.html')
